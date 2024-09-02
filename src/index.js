4dc6a33be3b13025ab8d86192bfdc06e23f989b7
Hanna Stseliukova <70797548+ByHannaStseliukova@users.noreply.github.com>
2024-09-02 20:49:58 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 
 module.exports = function toReadable (number) {
 let word = "";
-let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-let tenthsFromZeroToNinety = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-if (number === 0) {
-    return 'zero';
+
+let singleDigits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+let doubleDigits = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+let digitsBelowHundred = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+if (number < 10) {
+    word = singleDigits[number] + '';
 }
 else if (number < 20) {
-    word = fromZeroToNineteen[number] + '';
+    word = doubleDigits[number - 10] + ' ';
 }
 else if (number < 100) {
 let remainder = toReadable(number % 10);
-word = tenthsFromZeroToNinety[Math.trunc(number / 10)]+ '' + remainder;
+word = digitsBelowHundred[(number - number % 10) / 10 - 2]+ ' ' + remainder;
 }
 else if (number < 1000) {
-word = fromZeroToNineteen[Math.trunc(number / 100)] + ' Hundred ' + toReadable(number % 100);
+word = singleDigits[Math.trunc(number / 100)] + ' hundred ' + toReadable(number % 100);
 }
 return word;
 }
-let result = translate(number);
-return result.trim() + '.';
+
