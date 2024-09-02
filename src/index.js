66758debf6f4c67d6034ae1109a91688a545ccfe
Hanna Stseliukova <70797548+ByHannaStseliukova@users.noreply.github.com>
2024-09-02 23:18:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 
 module.exports = function toReadable (number) {
-    let converter = require ('number-to-words');
-    let translate = converter.toWords(number);
-    let replace = translate.replace ('-', ' ');
-    return replace;
+let word = "";
+let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let tenthsFromZeroToNinety = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+if (number === 0) {
+    return 'zero';
 }
+else if (number < 20) {
+    word = fromZeroToNineteen[number] + '';
+}
+else if (number < 100) {
+let remainder = toReadable(number % 10);
+word = tenthsFromZeroToNinety[Math.trunc(number / 10)]+ '' + remainder;
+}
+else if (number < 1000) {
+word = fromZeroToNineteen[Math.trunc(number / 100)] + ' Hundred ' + toReadable(number % 100);
+}
+return word;
+}
+let result = translate(number);
+return result.trim() + '.';
