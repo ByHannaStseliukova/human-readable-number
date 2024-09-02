150fa9a1b172f950e16bd59cec64f83962d6685a
Hanna Stseliukova <70797548+ByHannaStseliukova@users.noreply.github.com>
2024-09-02 15:58:45 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+
 module.exports = function toReadable (number) {
-    var converter = require ('number-to-words');
-    var translate = converter.toWords(number);
-    var replace = translate.replace ('-', ' ');
+    let converter = require ('number-to-words');
+    let translate = converter.toWords(number);
+    let replace = translate.replace ('-', ' ');
     return replace;
 }
